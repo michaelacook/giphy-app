b4f49212ef3a916ef77c086c33dd7a64faca62c8
Michael Cook <mcook0775@gmail.com>
2020-02-18 17:58:18 -0500

    Add error handling

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,8 @@
 function getTrending() {
     fetch(`${protocol}${url}trending?api_key=${key}&limit=${limit}`)
         .then(response => response.json())
-        .then(gifs => displayAllGifs(gifs));
+        .then(gifs => displayAllGifs(gifs))
+        .catch(error => displayErrorMessage());
 }
 
 // fetch gifs for search value
@@ -10,7 +11,8 @@ function getGifs(queryTerm) {
     fetch(`${protocol}${url}search?q=${queryTerm}&limit=${limit}&api_key=${key}`)
         .then(clearGifs())
         .then(res => res.json())
-        .then(gifs => displayAllGifs(gifs));
+        .then(gifs => displayAllGifs(gifs))
+        .catch(error => displayErrorMessage());
 }
 
 // generate an img element for a gif and append to DOM
@@ -43,17 +45,20 @@ function toggleTheme() {
     const light = 'https://img.icons8.com/android/24/000000/sun.png';
     return function() {
         const img = document.getElementById('toggleTheme'); 
+        const error = document.querySelector('#error h4');
         document.body.style.height = 'auto';   
         if (theme == 'dark') {
             img.src = dark;
             theme = 'light';
             document
                 .body.style.backgroundColor = 'white';
+            error.className = 'text-dark text-center';
         } else {
             img.src = light;
             theme = 'dark';
             document
                 .body.style.backgroundColor = '#191919';
+            error.className = 'text-light text-center';
         }
     }
 }
@@ -63,4 +68,13 @@ function toggleBodyHeight() {
     if (document.body.style.height == 'auto') {
         document.body.style.height = '100%';
     }
+}
+
+// error message for failed fetch 
+function displayErrorMessage() {
+    const theme = document.body.style.backgroundColor;
+    let className;
+    theme == 'white' ? className = 'text-dark text-center' : className = 'text-light text-center';
+    document.getElementById('error')
+        .innerHTML = `<h4 class="${className}">Something went wrong. Check your internet connection and try again.</h4>`;
 }
